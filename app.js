6d2f763f0455eb1bc71b22455517e440a9dc45fe
FengDenny <fengdennyy@gmail.com>
2020-07-24 01:15:03 -0700

    Error Handler

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const morgan = require('morgan')
 const userRoutes = require('./routes/userRoutes')
+const AppError = require('./utils/appError')
+const UniversalErrorHandler = require('./controller/errorController')
 
 const app = express();
 
@@ -15,11 +18,25 @@ app.use(express.urlencoded({
 
 // Body Parser END
 
+// Development logging 
+if (process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'))
+}
+
 
 // Routes
-
 app.use('/api/users', userRoutes)
 
+
+// Handleing unhandled routes
+// should be last part of our application
+// Build in express method(req.originalUrl)
+app.all('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Universal Error Handler for DB
+app.use(UniversalErrorHandler);
 
 
 module.exports = app;
