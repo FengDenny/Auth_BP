75e2b61ce9747cebfa309a6a9813d2a8d5aad011
FengDenny <fengdennyy@gmail.com>
2020-08-08 00:27:55 -0700

    Pug email template

---
+++ utils/email.js
@@ -0,0 +1,67 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+
+module.exports = class Email {
+  constructor(user, url, token) {
+    this.to = user.email;
+    this.firstName = user.firstName;
+    this.lastName = user.lastName;
+    this.url = url;
+    this.token = token;
+    this.from = `Denny Feng <${process.env.EMAIL_FROM}`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+    // Mailtrap
+    return nodemailer.createTransport({
+      // service: 'Gmail'
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+      firstName: this.firstName,
+      lastName: this.lastName,
+      to: this.to,
+      url: this.url,
+      token: this.token,
+      subject,
+    });
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+  async sendValidation() {
+    await this.send('emailVerify', 'Please Verify Your Email Address');
+  }
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes)'
+    );
+  }
+};
